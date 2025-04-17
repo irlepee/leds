9d278ce334ca444dfadb1e922854b696123cf3a2
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-04-17 13:08:50 -0700

    Todo configurado correctamente y funcionando, ventana con hola mundo jalando

--- src/main/java/application/app.java
+++ src/main/java/application/app.java
@@ -1,7 +1,22 @@
 package application;
 
-public class app {
-    public static void main (String[] args){
-        System.out.println("Hola mundo");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class app extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(utils.path.mainWindow));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
---
+++ src/main/java/utils/path.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class path {
+    public static final String mainWindow = "/mainWindow.fxml";
+}
